d24e20bd0fc7cbeeee921606ed9cf68ff331e924
Maxime Pelletier <pelletiermaxime@gmail.com>
2017-08-29 23:10:17 -0400

    Spawn miner tests

--- src/renderer/components/Mine/Benchmark.vue
+++ src/renderer/components/Mine/Benchmark.vue
@@ -6,6 +6,10 @@
       .row
         .col-6.
           BENCHMARK
+      .row
+        button(@click="spawnMiner") SPAWN MINER
+        button(@click="spawnMiner2") SPAWN MINER2
+        button(@click="spawnMiner3") SPAWN MINER3
 </template>
 <script>
   // import currencies from '@/store/currencies'
@@ -14,6 +18,11 @@
     QBtn, QCard, QCardMain, QIcon, QItemMain, QItemSide,
     QLayout, QSideLink, QToolbar
   } from 'quasar'
+
+  import path from 'path'
+
+  // const { spawn } = require('child_process')
+  const { exec } = require('child_process')
 
   // const store = new Store()
 
@@ -29,9 +38,44 @@
       QSideLink,
       QToolbar
     },
+
     data () {
       return {
         allCoinsInfo: {}
+      }
+    },
+
+    methods: {
+      spawnMiner () {
+        let minerPath = path.join(this.$electron.remote.app.getPath('appData'), '/MaxMiner', '/Miners')
+        exec(`${minerPath}/ccminer`, (err, stdout, stderr) => {
+          if (err) {
+            // node couldn't execute the command
+            return
+          }
+
+          // the *entire* stdout and stderr (buffered)
+          console.log(`stdout: ${stdout}`)
+          console.log(`stderr: ${stderr}`)
+        })
+      },
+      spawnMiner2 () {
+        let minerPath = path.join(this.$electron.remote.app.getPath('appData'), '/MaxMiner', '/Miners')
+        exec(`cmd /c ${minerPath}/ccminer`, function callback (error, stdout, stderr) {
+          console.log(error)
+          console.log(stdout)
+          console.log(stderr)
+          console.log('started console app')
+        })
+      },
+      spawnMiner3 () {
+        let minerPath = path.join(this.$electron.remote.app.getPath('appData'), '/MaxMiner', '/Miners')
+        exec(`start ${minerPath}/ccminer`, function callback (error, stdout, stderr) {
+          console.log(error)
+          console.log(stdout)
+          console.log(stderr)
+          console.log('started console app')
+        })
       }
     }
   }
