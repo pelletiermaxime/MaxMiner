8699a1e5df26b752136d4a992a909197beea29b1
Maxime Pelletier <pelletiermaxime@gmail.com>
2017-09-09 00:34:26 -0400

    Fix prohashing something returning nothing and round the values

--- src/renderer/store/pools/prohashing.js
+++ src/renderer/store/pools/prohashing.js
@@ -2,13 +2,14 @@ export default async (pool, http) => {
   let algos = {}
   let d = new Date()
   let startTime = d.setDate(d.getDate() - 1)
+  startTime -= 100000000
 
   let poolURLScrypt = `${pool.url}Scrypt&startTime=${startTime}`
   let result = await http.get(poolURLScrypt)
   let data = result.data[0]
   algos['scrypt'] = {
     estimate_current: 0,
-    actual_last24h: data.expectedPayoutBtc * 1000,
+    actual_last24h: (data.expectedPayoutBtc * 1000).toFixed(6),
     name: 'scrypt'
   }
 
@@ -17,7 +18,7 @@ export default async (pool, http) => {
   data = result.data[0]
   algos['x11'] = {
     estimate_current: 0,
-    actual_last24h: data.expectedPayoutBtc * 1000,
+    actual_last24h: (data.expectedPayoutBtc * 1000).toFixed(6),
     name: 'x11'
   }
 
