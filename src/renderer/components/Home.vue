ea344cfebecb603c9632df171fec5efe14a5b77c
Marc-Antoine Bouchard Marceau <marc-antoine.bm@libeo.com>
2017-09-11 13:04:49 -0400

    Welcome page un tout petit peu moins laid

--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -1,6 +1,19 @@
 <template>
-<div>
-  Welcome!
-  Home!
-</div>
+    <div>
+        <p>
+            MaxMiner!
+        </p>
+    </div>
 </template>
+
+<style scoped>
+    div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: calc(100vh - 98px);
+    }
+    div p {
+        font-size: 4em;
+    }
+</style>
