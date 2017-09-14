e93efd83977c95c9ce0cfe5361055a7efcd0d466
Maxime Pelletier <pelletiermaxime@gmail.com>
2017-09-14 00:20:40 -0400

    [Pools] Add back YiiMP now that it's back

--- src/renderer/store/poollist.js
+++ src/renderer/store/poollist.js
@@ -22,11 +22,11 @@ export default {
     name: 'zpool',
     type: 'yiimp',
     url: 'http://zpool.ca'
+  },
+  YiiMP: {
+    logo: 'http://yiimp.ccminer.org/favicon.ico',
+    name: 'YiiMP',
+    type: 'yiimp',
+    url: 'http://yiimp.ccminer.org'
   }
-  // YiiMP: {
-  //   logo: 'http://yiimp.ccminer.org/favicon.ico',
-  //   name: 'YiiMP',
-  //   type: 'yiimp',
-  //   url: 'http://yiimp.ccminer.org'
-  // }
 }
